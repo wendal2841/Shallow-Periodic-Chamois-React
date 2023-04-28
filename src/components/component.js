9b03a3a61a5ec7199ee805bc3832c775d7c00983
wendal2841 <wendal@bk.ru>
2023-04-28 11:19:43 +0300

    Commit from TeleportHQ

--- src/components/component.js
+++ src/components/component.js
@@ -9,9 +9,9 @@ const AppComponent = (props) => {
         <div className="component-frame34549">
           <div className="component-frame206808">
             <span className="component-text MobileH2">
-              <span>How to become a Manager?</span>
+              How to become a Manager? 1111
             </span>
-            <span className="component-text02 ParagraphP16(default)">
+            <span className="component-text01 ParagraphP16(default)">
               <span>
                 A truck driver is a professional who drives a commercial vehicle
                 to transport goods from one location to another.
@@ -19,23 +19,23 @@ const AppComponent = (props) => {
             </span>
           </div>
           <div className="component-frame34548">
-            <span className="component-text04 MobileH3">
+            <span className="component-text03 MobileH3">
               <span>Hard skills:</span>
             </span>
             <div className="component-frame34552">
               <div className="component-frame206810">
-                <span className="component-text06">
+                <span className="component-text05">
                   <span>1.</span>
                 </span>
-                <span className="component-text08 ParagraphP16(default)">
+                <span className="component-text07 ParagraphP16(default)">
                   <span>Knowledge of road safety and traffic laws.</span>
                 </span>
               </div>
               <div className="component-frame206809">
-                <span className="component-text10">
+                <span className="component-text09">
                   <span>2.</span>
                 </span>
-                <span className="component-text12 ParagraphP16(default)">
+                <span className="component-text11 ParagraphP16(default)">
                   <span>
                     Proficiency in operating and maintaining a commercial
                     vehicle.
@@ -43,20 +43,20 @@ const AppComponent = (props) => {
                 </span>
               </div>
               <div className="component-frame206811">
-                <span className="component-text14">
+                <span className="component-text13">
                   <span>3.</span>
                 </span>
-                <span className="component-text16 ParagraphP16(default)">
+                <span className="component-text15 ParagraphP16(default)">
                   <span>
                     Ability to read and interpret maps and directions.
                   </span>
                 </span>
               </div>
               <div className="component-frame206812">
-                <span className="component-text18">
+                <span className="component-text17">
                   <span>4.</span>
                 </span>
-                <span className="component-text20 ParagraphP16(default)">
+                <span className="component-text19 ParagraphP16(default)">
                   <span>
                     Knowledge of basic mechanics to handle minor vehicle
                     repairs.
@@ -64,10 +64,10 @@ const AppComponent = (props) => {
                 </span>
               </div>
               <div className="component-frame206813">
-                <span className="component-text22">
+                <span className="component-text21">
                   <span>5.</span>
                 </span>
-                <span className="component-text24 ParagraphP16(default)">
+                <span className="component-text23 ParagraphP16(default)">
                   <span>
                     Proficiency in using communication devices like radios and
                     GPS systems.
@@ -77,15 +77,15 @@ const AppComponent = (props) => {
             </div>
           </div>
           <div className="component-frame345491">
-            <span className="component-text26">
+            <span className="component-text25">
               <span>Soft skills:</span>
             </span>
             <div className="component-frame34551">
               <div className="component-frame2068101">
-                <span className="component-text28">
+                <span className="component-text27">
                   <span>1.</span>
                 </span>
-                <span className="component-text30 ParagraphP16(default)">
+                <span className="component-text29 ParagraphP16(default)">
                   <span>
                     Good communication skills to interact with clients and
                     co-workers.
@@ -93,10 +93,10 @@ const AppComponent = (props) => {
                 </span>
               </div>
               <div className="component-frame2068091">
-                <span className="component-text32">
+                <span className="component-text31">
                   <span>2.</span>
                 </span>
-                <span className="component-text34 ParagraphP16(default)">
+                <span className="component-text33 ParagraphP16(default)">
                   <span>
                     Ability to remain calm under pressure and handle unexpected
                     situations.
@@ -104,28 +104,28 @@ const AppComponent = (props) => {
                 </span>
               </div>
               <div className="component-frame2068111">
-                <span className="component-text36">
+                <span className="component-text35">
                   <span>3.</span>
                 </span>
-                <span className="component-text38 ParagraphP16(default)">
+                <span className="component-text37 ParagraphP16(default)">
                   <span>
                     Time management skills to meet deadlines and schedules.
                   </span>
                 </span>
               </div>
               <div className="component-frame2068121">
-                <span className="component-text40">
+                <span className="component-text39">
                   <span>4.</span>
                 </span>
-                <span className="component-text42 ParagraphP16(default)">
+                <span className="component-text41 ParagraphP16(default)">
                   <span>Patience and focus to drive for long hours.</span>
                 </span>
               </div>
               <div className="component-frame2068131">
-                <span className="component-text44">
+                <span className="component-text43">
                   <span>5.</span>
                 </span>
-                <span className="component-text46">
+                <span className="component-text45">
                   <span>
                     Strong work ethic and responsibility to ensure cargo safety
                     and timely delivery.
