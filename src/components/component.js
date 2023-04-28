4d26964d0a218cc6b59aea07170309154af2cc86
wendal2841 <wendal@bk.ru>
2023-04-28 11:16:11 +0300

    Commit from TeleportHQ

---
+++ src/components/component.js
@@ -0,0 +1,143 @@
+import React from 'react'
+
+import './component.css'
+
+const AppComponent = (props) => {
+  return (
+    <div className="component-container">
+      <div className="component-frame-howmuchdoesamanagermakein-new-york-usa">
+        <div className="component-frame34549">
+          <div className="component-frame206808">
+            <span className="component-text MobileH2">
+              <span>How to become a Manager?</span>
+            </span>
+            <span className="component-text02 ParagraphP16(default)">
+              <span>
+                A truck driver is a professional who drives a commercial vehicle
+                to transport goods from one location to another.
+              </span>
+            </span>
+          </div>
+          <div className="component-frame34548">
+            <span className="component-text04 MobileH3">
+              <span>Hard skills:</span>
+            </span>
+            <div className="component-frame34552">
+              <div className="component-frame206810">
+                <span className="component-text06">
+                  <span>1.</span>
+                </span>
+                <span className="component-text08 ParagraphP16(default)">
+                  <span>Knowledge of road safety and traffic laws.</span>
+                </span>
+              </div>
+              <div className="component-frame206809">
+                <span className="component-text10">
+                  <span>2.</span>
+                </span>
+                <span className="component-text12 ParagraphP16(default)">
+                  <span>
+                    Proficiency in operating and maintaining a commercial
+                    vehicle.
+                  </span>
+                </span>
+              </div>
+              <div className="component-frame206811">
+                <span className="component-text14">
+                  <span>3.</span>
+                </span>
+                <span className="component-text16 ParagraphP16(default)">
+                  <span>
+                    Ability to read and interpret maps and directions.
+                  </span>
+                </span>
+              </div>
+              <div className="component-frame206812">
+                <span className="component-text18">
+                  <span>4.</span>
+                </span>
+                <span className="component-text20 ParagraphP16(default)">
+                  <span>
+                    Knowledge of basic mechanics to handle minor vehicle
+                    repairs.
+                  </span>
+                </span>
+              </div>
+              <div className="component-frame206813">
+                <span className="component-text22">
+                  <span>5.</span>
+                </span>
+                <span className="component-text24 ParagraphP16(default)">
+                  <span>
+                    Proficiency in using communication devices like radios and
+                    GPS systems.
+                  </span>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="component-frame345491">
+            <span className="component-text26">
+              <span>Soft skills:</span>
+            </span>
+            <div className="component-frame34551">
+              <div className="component-frame2068101">
+                <span className="component-text28">
+                  <span>1.</span>
+                </span>
+                <span className="component-text30 ParagraphP16(default)">
+                  <span>
+                    Good communication skills to interact with clients and
+                    co-workers.
+                  </span>
+                </span>
+              </div>
+              <div className="component-frame2068091">
+                <span className="component-text32">
+                  <span>2.</span>
+                </span>
+                <span className="component-text34 ParagraphP16(default)">
+                  <span>
+                    Ability to remain calm under pressure and handle unexpected
+                    situations.
+                  </span>
+                </span>
+              </div>
+              <div className="component-frame2068111">
+                <span className="component-text36">
+                  <span>3.</span>
+                </span>
+                <span className="component-text38 ParagraphP16(default)">
+                  <span>
+                    Time management skills to meet deadlines and schedules.
+                  </span>
+                </span>
+              </div>
+              <div className="component-frame2068121">
+                <span className="component-text40">
+                  <span>4.</span>
+                </span>
+                <span className="component-text42 ParagraphP16(default)">
+                  <span>Patience and focus to drive for long hours.</span>
+                </span>
+              </div>
+              <div className="component-frame2068131">
+                <span className="component-text44">
+                  <span>5.</span>
+                </span>
+                <span className="component-text46">
+                  <span>
+                    Strong work ethic and responsibility to ensure cargo safety
+                    and timely delivery.
+                  </span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AppComponent
