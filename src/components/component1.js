660681dcfbaa5611394f1e973c5e9b3081051547
wendal2841 <wendal@bk.ru>
2023-04-28 12:48:24 +0300

    Commit from TeleportHQ

---
+++ src/components/component1.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './component1.css'
+
+const Component1 = (props) => {
+  return (
+    <div className="component1-container">
+      <button className="component1-buttons button">
+        <span className="component1-text">
+          <span>Search all 5,331 vacancies</span>
+        </span>
+      </button>
+    </div>
+  )
+}
+
+export default Component1
