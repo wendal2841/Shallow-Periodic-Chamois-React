f60398b1bf22768ccf56f8028175fb919c2a7d2b
wendal2841 <wendal@bk.ru>
2023-04-28 12:55:52 +0300

    Commit from TeleportHQ

---
+++ src/components/component2.js
@@ -0,0 +1,108 @@
+import React from 'react'
+
+import './component2.css'
+
+const Component2 = (props) => {
+  return (
+    <div className="component2-container">
+      <div className="component2-headerwithselectedtab">
+        <div className="component2-header">
+          <div className="component2-frame34319">
+            <div className="component2-logo">
+              <div className="component2-logo1">
+                <div className="component2-logotypeshort">
+                  <div className="component2-content">
+                    <img
+                      alt="ecologygreenI161"
+                      src="/playground_assets/ecologygreeni161-vpjg.svg"
+                      className="component2-ecologygreen"
+                    />
+                    <img
+                      alt="blueI161"
+                      src="/playground_assets/bluei161-qmfk.svg"
+                      className="component2-blue"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="component2-search">
+              <div className="component2-inputs">
+                <div className="component2-frame573">
+                  <img
+                    alt="searchI161"
+                    src="/playground_assets/searchi161-s8vn.svg"
+                    className="component2-search1"
+                  />
+                  <span className="component2-text ParagraphP16(default)1">
+                    <span>Manager</span>
+                  </span>
+                </div>
+              </div>
+              <div className="component2-inputs1">
+                <div className="component2-frame5731">
+                  <img
+                    alt="mapmarkerI161"
+                    src="/playground_assets/mapmarkeri161-c4xui.svg"
+                    className="component2-mapmarker"
+                  />
+                  <span className="component2-text02 ParagraphP16(default)1">
+                    <span>New York</span>
+                  </span>
+                </div>
+              </div>
+              <button className="component2-buttons">
+                <span className="component2-text04">
+                  <span>Find</span>
+                </span>
+              </button>
+              <img
+                alt="microphoneI161"
+                src="/playground_assets/microphonei161-apq.svg"
+                className="component2-microphone"
+              />
+            </div>
+            <div className="component2-frame574">
+              <button className="component2-buttons1">
+                <img
+                  alt="staroutlineI161"
+                  src="/playground_assets/staroutlinei161-m2dr.svg"
+                  className="component2-staroutline"
+                />
+                <span className="component2-text06">
+                  <span>Favourites</span>
+                </span>
+              </button>
+              <button className="component2-buttons2">
+                <img
+                  alt="usercircleI161"
+                  src="/playground_assets/usercirclei161-b45.svg"
+                  className="component2-usercircle"
+                />
+                <span className="component2-text08">
+                  <span>Profile</span>
+                </span>
+              </button>
+              <div className="component2-frame605">
+                <img
+                  alt="Rectangle181I161"
+                  src="/playground_assets/rectangle181i161-f4pw-200w.png"
+                  className="component2-rectangle181"
+                />
+              </div>
+              <button className="component2-buttons3">
+                <img
+                  alt="menuburgerI161"
+                  src="/playground_assets/menuburgeri161-af3.svg"
+                  className="component2-menuburger"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Component2
